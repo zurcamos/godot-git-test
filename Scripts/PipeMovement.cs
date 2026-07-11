d2ed9436644ca23d4ff852e85fe2559a23593702
zurcamos <zurcamos17@gmail.com>
2026-07-11 15:59:47 -0400

    smt new

--- Scripts/PipeMovement.cs
+++ Scripts/PipeMovement.cs
@@ -7,26 +7,29 @@ public partial class PipeMovement : RigidBody2D
 	[Export] private float minMaxOffset;
 
 	[Export] private float speed;
+	[Export] private Area2D score { get; set; }
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		AddToGroup("Death");
-		offset = (float)GD.RandRange(-minMaxOffset,minMaxOffset);
+		offset = (float)GD.RandRange(-minMaxOffset, minMaxOffset);
 		Vector2 pos = Position;
 		pos.Y = offset;
 		Position = pos;
+		GetNode<Area2D>("%killZone").Connect("body_entered", new Callable(this, "KILLME"));
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		LinearVelocity = new Vector2(speed,0);
+		LinearVelocity = new Vector2(speed, 0);
 
 		// MoveAndSlide();
 	}
 
-	public void KILLME() {
+	public void KILLME()
+	{
 		GD.Print("I'm dead skullemoji");
 		QueueFree();
 	}
