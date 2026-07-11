d35548c8c6f9dc14377e914b6d5494b765b25783
zurcamos <zurcamos17@gmail.com>
2026-07-11 18:56:54 -0400

    end screen works L

--- Scripts/GameOver.cs
+++ Scripts/GameOver.cs
@@ -3,8 +3,10 @@ using System;
 
 public partial class GameOver : Control
 {
+
 	public void RETYR()
 	{
+		GD.Print("btn pressed");
 		GetTree().ChangeSceneToFile("res://Scenes/FlappyBird.tscn");
 	}
 
