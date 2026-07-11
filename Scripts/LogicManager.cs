54b4478f1e08b09f6e973705b76a8cec4103b16e
TripTurner <alexpwoolston@gmail.com>
2026-07-11 17:43:40 -0400

    Audio

--- Scripts/LogicManager.cs
+++ Scripts/LogicManager.cs
@@ -3,11 +3,13 @@ using System;
 
 public partial class LogicManager : Node2D
 {
+	[Export] private AudioStreamPlayer audioPlayer;
 	[Export] public Label scoreText;
 	private int score = 0;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		audioPlayer = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,6 +23,12 @@ public partial class LogicManager : Node2D
 	public void updateScore()
 	{
 		score += 1;
+<<<<<<< Updated upstream
 		scoreText.Text = $"Score: {score}😘";
+=======
+		scoreText.Text = $"Score: {score}";
+		audioPlayer.PitchScale = (float)GD.RandRange(0.9,1.1);
+		audioPlayer.Play();
+>>>>>>> Stashed changes
 	}
 }
