3db94ab818af8f25bc63da42d39ae50f830f461c
TripTurner <alexpwoolston@gmail.com>
2026-07-11 15:10:58 -0400

    Made Pipe Instantiator

---
+++ Scripts/PipeSpawner.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class PipeSpawner : Node2D
+{
+	[Export] public PackedScene pipe {get; set;}
+	private float spawnTimer;
+	[Export] public float spawnTime = 3;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		spawnTimer += (float)delta;
+		if (spawnTimer>=spawnTime) {
+			spawnTimer = 0;
+			Node2D newPipe = pipe.Instantiate<Node2D>();
+			newPipe.GlobalPosition = GlobalPosition;
+			AddChild(newPipe);
+		}
+	}
+}
